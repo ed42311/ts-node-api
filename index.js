86c327ff370ab5b399cddef3c2ae191f567293a6
Edward Weymouth <edward.weymouth@murdochs.com>
2021-07-21 12:55:45 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+// Instantiating the app
+const app = express();
+
+// Set the port
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.json({message: "OK"})
+})
+
+app.get('/hello/:name', (req, res) => {
+    res.json({message: `Hello ${req.params.name}`})
+})
+
+app.listen(port, () => {
+    console.log('Server running on port: ' + port);
+})
