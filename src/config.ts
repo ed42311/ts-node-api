aff09dbccf72eeae7eb5fbc3cefd7cf239e5e683
Edward Weymouth <edwardweymouth@Edwards-iMac.local>
2021-07-31 14:40:54 -0600

    reject unauthorized

--- src/config.ts
+++ src/config.ts
@@ -2,14 +2,15 @@
 import { Pool, PoolConfig } from "pg";
 import { config } from "dotenv";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 config();
 
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
-const isProduction = process.env.NODE_ENV === "production";
 
 const poolConfig: PoolConfig = {
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  ssl: isProduction,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
 };
 
 const pool = new Pool(poolConfig);
